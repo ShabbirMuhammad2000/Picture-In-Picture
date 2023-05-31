17433a3605162ee0de0adf7c8aa0ae5136b0f29c
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-05-31 15:57:15 -0400

    Made the prompt show up when user stops screen sharing and clicks start button agian

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const videoElement = document.getElementById('video')
-const screenContainer = document.getSelection('screenContainer')
-const button = document.getElementById('button')
-let mediaStream
+const videoElement = document.getElementById('video');
+const screenContainer = document.getSelection('screenContainer');
+const button = document.getElementById('button');
+let mediaStream;
 
 // Prompt to select media stream, pass to video element, then play 
 async function selectMediaStream() {
   try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    mediaStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = mediaStream;
     videoElement.onloadedmetadata = () => {
-      videoElement.play()
-    }
+      videoElement.play();
+    };
     mediaStream.getVideoTracks()[0].onended = () => {
-      // Prompt the user agian when the screen stops sharing
-      selectMediaStream()
-    }
+      // Reset the mediaStream object when screen sharing stops
+      mediaStream = null;
+      // Prompt the user to share screen again when the screen stops sharing
+      button.disabled = false;
+    };
   } catch (error) {
     // Catch Error Here
-    console.log('whoops, error here:', error)
+    console.log('whoops, error here:', error);
+    mediaStream = null; // Reset the mediaStream object
+    button.disabled = false; // Enable the start button to show the prompt again
   }
 }
 
@@ -32,21 +36,22 @@ function showScreenContainer() {
 
 button.addEventListener('click', async () => {
   // Disable Button
-  button.disabled = true
+  button.disabled = true;
+
   // Start Picture in Picture
   try {
-    await videoElement.requestPictureInPicture()
-
-     //Reset Button
-  button.disabled = false
+    if (mediaStream && mediaStream.active) {
+      await videoElement.requestPictureInPicture();
+    } else {
+      await selectMediaStream();
+      await videoElement.requestPictureInPicture();
+    }
   } catch (error) {
-    // Prompt the user id the request is deined
-    selectMediaStream()
-
-    //Reset Button
-  button.disabled = false
+    // Catch Error Here
+    console.log('whoops, error here:', error);
+    button.disabled = false; // Enable the start button to show the prompt again
   }
-})
+});
 
 // On Load
 selectMediaStream();
